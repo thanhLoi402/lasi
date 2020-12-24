a91f22235b31d72cf01cfe7c62b719d3e03207ae
= <=>
2020-12-24 16:49:43 +0700

    chage file

--- change_format_data.py
+++ change_format_data.py
@@ -15,35 +15,34 @@ def change_format(in_path_dir, in_path_log, old_format, new_format):
     arr = os.listdir(in_path_dir)
     patten = ''
     for i in arr:
-        if i == 'getLogCharge.sh':
-            f = open(i, 'r')
-            f_print = open(in_path_log + i, 'w')
-            for x in f:
-                if ' ' in x:
-                    patten = ' '
-                elif ';' in x:
-                    patten = ';'
-                elif ',' in x:
-                    patten = ','
-                else:
-                    patten = ';'
-                x = x.replace(' ',';').replace(',',';')
-                arr_x = x.split(';')
-                base_data = arr_x[0:len(base_template)]
-                for_data = arr_x[len(base_template):len(arr_x)]
-                print('base_data:' , base_data)
-                print('for_data:' , for_data)
-                j = 0
-                while j < len(for_data):
-                    new_line = '';
-                    for idx in new_arr:
-                        if idx in base_template:
-                            new_line+= base_data[base_template.index(idx)] + patten
-                        if idx in for_template:
-                            new_line+= for_data[j + for_template.index(idx)] + patten
-                    print("new_line:"+ new_line)
-                    f_print.write(new_line + '\n')
-                    j = j + len_for
-            f_print.close()
-            os.remove(i)
-            print('read and remove done file ' + in_path_dir + i + ' to ' + in_path_log + i)
+        f = open(i, 'r')
+        f_print = open(in_path_log + i, 'w')
+        for x in f:
+            if ' ' in x:
+                patten = ' '
+            elif ';' in x:
+                patten = ';'
+            elif ',' in x:
+                patten = ','
+            else:
+                patten = ';'
+            x = x.replace(' ',';').replace(',',';')
+            arr_x = x.split(';')
+            base_data = arr_x[0:len(base_template)]
+            for_data = arr_x[len(base_template):len(arr_x)]
+            print('base_data:' , base_data)
+            print('for_data:' , for_data)
+            j = 0
+            while j < len(for_data):
+                new_line = '';
+                for idx in new_arr:
+                    if idx in base_template:
+                        new_line+= base_data[base_template.index(idx)] + patten
+                    if idx in for_template:
+                        new_line+= for_data[j + for_template.index(idx)] + patten
+                print("new_line:"+ new_line)
+                f_print.write(new_line + '\n')
+                j = j + len_for
+        f_print.close()
+        os.remove(i)
+        print('read and remove done file ' + in_path_dir + i + ' to ' + in_path_log + i)
